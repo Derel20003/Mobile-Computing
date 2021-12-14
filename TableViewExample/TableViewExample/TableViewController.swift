a36fcc6bd9f9f1ec887487d79db98f6d08a1f930
Derel20003 <57949371+Derel20003@users.noreply.github.com>
2021-12-14 12:37:35 +0100

    Added TableViewExample

--- TableViewExample/TableViewExample/TableViewController.swift
+++ TableViewExample/TableViewExample/TableViewController.swift
@@ -8,9 +8,15 @@
 import UIKit
 
 class TableViewController: UITableViewController {
+    
+    var guesses = [Int]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        for i in 1...15 {
+            guesses.append(i)
+        }
 
         // Uncomment the following line to preserve selection between presentations
         // self.clearsSelectionOnViewWillAppear = false
@@ -23,23 +29,23 @@ class TableViewController: UITableViewController {
 
     override func numberOfSections(in tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
-        return 0
+        return 1
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return 0
+        return guesses.count
     }
 
-    /*
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+        let cell = tableView.dequeueReusableCell(withIdentifier: "guess", for: indexPath)
 
         // Configure the cell...
+        
+        cell.textLabel?.text = "\(guesses[indexPath.row])"
 
         return cell
     }
-    */
 
     /*
     // Override to support conditional editing of the table view.
